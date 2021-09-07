86348639181819cc7c2e6f0bba7cc9146437c3d7
amrn1991 <amrfst91@gmail.com>
2021-09-07 20:09:55 +0430

    designed home page

---
+++ src/data.js
@@ -0,0 +1,46 @@
+const data = {
+  products: [
+    {
+      id: 1,
+      name: "Slim Shirt",
+      category: "shirts",
+      image: "/images/Park-Avenue-Shirt.jpg",
+      brand: "Park Avenue",
+      price: 60,
+      rating: 4.2,
+      numReviews: 10,
+    },
+    {
+      id: 2,
+      name: "Fit Shirt",
+      category: "shirts",
+      image: "/images/Park-Avenue-Shirt.jpg",
+      brand: "Park Avenue",
+      price: 50,
+      rating: 4.4,
+      numReviews: 15,
+    },
+    {
+      id: 3,
+      name: "Long Pants",
+      category: "pants",
+      image: "/images/Park-Avenue-Shirt.jpg",
+      brand: "Park Avenue",
+      price: 75,
+      rating: 4.1,
+      numReviews: 13,
+    },
+    {
+      id: 4,
+      name: "Baggy Jeans",
+      category: "pants",
+      image: "/images/Park-Avenue-Shirt.jpg",
+      brand: "Park Avenue",
+      price: 120,
+      rating: 4.6,
+      numReviews: 44,
+    },
+  ],
+};
+
+export default data;
