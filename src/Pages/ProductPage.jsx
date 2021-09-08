249e0db286a9a90276baef0d5088b21e71fe2172
amrn1991 <amrfst91@gmail.com>
2021-09-08 18:52:24 +0430

    designed product detail page

--- src/Pages/ProductPage.jsx
+++ src/Pages/ProductPage.jsx
@@ -1,11 +1,51 @@
 import React from "react";
 import data from "./../data";
+import { Link } from "react-router-dom";
 
 const ProductPage = ({ match }) => {
   const product = data.products.find((pro) => pro._id === match.params.id);
   return (
     <div>
-      <h3>{product.name}</h3>
+      <div className="back-btn">
+        <Link to="/">Back to products</Link>
+      </div>
+      <div className="details">
+        <div className="details-image">
+          <img src={product.image} alt={product.name} />
+        </div>
+        <div className="details-info">
+          <ul>
+            <li>
+              <h4>{product.name}</h4>
+            </li>
+            <li>
+              {product.rating} Stars ({product.numReviews} Reviews)
+            </li>
+            <li>
+              <em>${product.price}</em>
+            </li>
+            <li>{product.description}</li>
+          </ul>
+        </div>
+        <div className="details-action">
+          <ul>
+            <li>Price: ${product.price}</li>
+            <li>Status: {product.status}</li>
+            <li>
+              Qty:{" "}
+              <select>
+                <option value="">1</option>
+                <option value="">2</option>
+                <option value="">3</option>
+                <option value="">4</option>
+              </select>
+            </li>
+            <li>
+              <button className="button primary">Add to Cart</button>
+            </li>
+          </ul>
+        </div>
+      </div>
     </div>
   );
 };
